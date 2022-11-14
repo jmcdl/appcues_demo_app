6880fcb8f3c70f24266d51fb815c302a946e150f
James McDonell <james.mcdonell@appcues.com>
2022-11-14 12:22:22 -0500

    add a button for goal

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import squirrel from "./resources/squirrel.jpg";
 
-/* 
-  feel free to add/remove any HTML you would like. Just remember that all content
-  must be returned nested inside a single element (in this case, the .app-content div)
-*/
+const ANGRY_MOOD = "angry :/";
+const HAPPY_MOOD = "happy :)";
 
 const Home = () => {
+  const [mood, setMood] = useState(ANGRY_MOOD);
   return (
     <div className="app-content">
       <h1>Welcome to this feature-rich test application</h1>
@@ -15,6 +14,16 @@ const Home = () => {
         src={squirrel}
         alt="a cute little squirrel peering at you from the tall grass"
       ></img>
+      <button
+        className="hello-button"
+        disabled={mood === HAPPY_MOOD}
+        onClick={() => setMood(HAPPY_MOOD)}
+      >
+        Say Hello
+      </button>
+      <div>
+        Mr squirrel is: <span className="mood-text">{mood}</span>
+      </div>
     </div>
   );
 };
