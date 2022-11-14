af5ef617e67f879fae02d0865078ccadb4a623fa
James McDonell <james.mcdonell@appcues.com>
2022-11-14 08:51:56 -0500

    use Date to get a unique userId

--- src/App.js
+++ src/App.js
@@ -12,12 +12,14 @@ like Appcues.on so that they will be called every time the page loads.
 */
 
 const App = () => {
-  if (!window.localStorage.currentUser) { // persists the current user across page refresh
-    window.localStorage.setItem("currentUser", "testUser"); // if no current user, set userID to "testUser"
+  if (!window.localStorage.currentUser) {
+    // persists the current user across page refresh
+    window.localStorage.setItem("currentUser", Date.now().toString()); // if no current user, set userID to "testUser"
   }
 
-  if (!window.localStorage.timestamp) { // persists the timestamp across refresh
-    window.localStorage.setItem("timestamp", Date.now()); // if no timestamp, sets it to now
+  if (!window.localStorage.timestamp) {
+    // persists the timestamp across refresh
+    window.localStorage.setItem("timestamp", Date.now().toString()); // if no timestamp, sets it to now
   }
 
   const userId = window.localStorage.currentUser;
